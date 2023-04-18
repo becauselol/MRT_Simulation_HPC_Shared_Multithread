e47f9b2a1934aec3a703a1b4af18b3a69a8b9437
becauselol <63877847+becauselol@users.noreply.github.com>
2023-04-18 16:51:05 +0800

    Full 12 thread test

--- src/simulation_functions/simul_functions.jl
+++ src/simulation_functions/simul_functions.jl
@@ -5,57 +5,47 @@
 function simulate_timestep!(time, metro, timestep=0.1, stop_spawn = 1440)
 	# for all stations we try spawn event
 	@debug "$(time)"
-	spawn_count = 0
+	
+	spawn_count = zeros(nthreads())
 	if (time <= stop_spawn)
-		hour = convert(Int32, floor(time/60))
-		for (station_id, station) in metro.stations 
-			for (target_id, target) in metro.stations 
-				if (target_id == station_id) 
-					continue 
+		hour = convert(Int64, floor(time/60))
+		@threads for station_id in 1:length(metro.stations)
+		    begin
+		        station = metro.stations[station_id]  
+				for (target_id, target) in metro.stations 
+					if (target_id == station_id) 
+						continue 
+					end 
+					if !(haskey(station.spawn_rate, target_id))
+						continue 
+					end 
+					spawn_count[threadid()] += event_spawn_commuters!(time, hour, metro, station_id, target_id, timestep)
 				end 
-				if !(haskey(station.spawn_rate, target_id))
-					continue 
-				end 
-				spawn = event_spawn_commuters!(time, hour, metro, station_id, target_id, timestep)
-				spawn_count = spawn_count + spawn
-			end 
+			end
 		end 
 	end 
-	@debug "done spawning $(spawn_count)"
 
 	# we check the event queue for any events to process
-	term_station_count = 0
-	for (station_id, station) in metro.stations 
+	@threads for station_id in 1:length(metro.stations)
+	    begin
+	        station = metro.stations[station_id] 
 
-		while size(station.event_queue)[1] > 0 && station.event_queue[1].time <= time 
-			station.event_queue, new_event = update_after_pop(station.event_queue)
+			while size(station.event_queue)[1] > 0 && station.event_queue[1].time <= time 
+				station.event_queue, new_event = update_after_pop(station.event_queue)
 
-			if new_event.event_type
-				@assert new_event.station == station_id 
- 
-				event_train_reach_station!(time, metro, new_event.train, new_event.station)
-			else 
-				@assert new_event.station == station_id
+				if new_event.event_type
+					@assert new_event.station == station_id 
+	 
+					event_train_reach_station!(time, metro, new_event.train, new_event.station)
+				else 
+					@assert new_event.station == station_id
 
-				event_train_leave_station!(time, metro, new_event.train, new_event.station)
+					event_train_leave_station!(time, metro, new_event.train, new_event.station)
+				end 
+
 			end 
-
-		end 
+		end
 	end 
-	@debug "trains term count: $(term_station_count)"
-
-	train_count = 0
-	for (train_id, train) in metro.trains 
-		train_count += get_shared_vector_count(train.commuters) 
-	end 
-	@debug "train commuters : $(train_count)"
-
-	station_count = 0
-	for (station_id, station) in metro.stations 
-		station_count += get_shared_vector_count(station.commuters["waiting"]) 
-		station_count += get_shared_vector_count(station.commuters["terminating"]) 
-	end 
-	@debug "station commuters : $(station_count)"
 
 	# process them accordingly
 
@@ -63,18 +53,18 @@ function simulate_timestep!(time, metro, timestep=0.1, stop_spawn = 1440)
 		station.event_queue = update_event_queue!(station.event_queue, station.event_buffer)
 	end 
 
-	@debug "updated queue"
 
 	# we then just terminate at the station
-	term_count = 0
-	for (station_id, station) in metro.stations 
-		term_count += event_terminate_commuters!(time, metro, station_id)
+	term_count = zeros(nthreads())
+	@threads for station_id in 1:length(metro.stations)
+	    begin
+	        station = metro.stations[station_id] 
+			term_count[threadid()] += event_terminate_commuters!(time, metro, station_id)
+		end
 	end 
 
-	@debug "terminate people $(term_count)"
-
 	# @assert term_count == term_station_count
-	return spawn_count, term_count
+	return sum(spawn_count), sum(term_count)
 end
 
 function get_shared_vector_count(shared_vector)
@@ -131,9 +121,10 @@ function simulate!(start_time, max_time, metro, timestep=0.1)
 		spawn_cum += spawn 
 		term_cum += term 
 
-		# @info "spawned: $(spawn_cum), terminated: $(term_cum) "
+		
 		time += timestep 
 	end 
+	@info "spawned: $(spawn_cum), terminated: $(term_cum) "
 end 
 
 # function simulate!(max_time, metro, event_queue, data_store)
